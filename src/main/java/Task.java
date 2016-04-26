127bf1356c3b665754de4b44ebc358682d9df489
Mike <mike@epicodus.com>
2016-04-26 16:53:37 -0700

    add task all list

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Task {
   private String mDescription;
   private boolean mCompleted;
   private LocalDateTime mCreatedAt;
+  private static ArrayList<Task> instances = new ArrayList<Task>();
 
   public Task(String description) {
     mDescription = description;
     mCompleted = false;
     mCreatedAt = LocalDateTime.now();
+    instances.add(this);
   }
 
   public String getDescription() {
@@ -21,5 +24,9 @@ public class Task {
 
   public LocalDateTime getCreatedAt() {
     return mCreatedAt;
+  }
+
+  public static ArrayList<Task> all() {
+    return instances;
   }
 }
