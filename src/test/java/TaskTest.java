46a67d3d277f75bb5eb88071961593584d2305ba
Mike <mike@epicodus.com>
2016-04-26 16:52:47 -0700

    add completed and createdat

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,18 +1,31 @@
+import java.time.LocalDateTime;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TaskTest {
 
   @Test
-  public void Task_instantiatesCorrectly_true() {
+  public void task_instantiatesCorrectly_true() {
     Task myTask = new Task("Mow the lawn");
     assertEquals(true, myTask instanceof Task);
   }
 
   @Test
-  public void Task_instantiatesWithDescription_String() {
+  public void task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
   }
-  
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
 }
