81574ee34e664364ed7285320475b465c522fef6
Mike <mike@epicodus.com>
2016-04-26 18:00:23 -0700

    update existing front-end to work with restful routing

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -26,6 +26,7 @@ public class AppTest extends FluentTest {
   @Test
   public void taskIsCreatedTest() {
     goTo("http://localhost:4567/");
+    click("a", withText("Add a new task"));
     fill("#description").with("Mow the lawn");
     submit(".btn");
     assertThat(pageSource()).contains("Your task has been saved.");
@@ -33,22 +34,22 @@ public class AppTest extends FluentTest {
 
   @Test
   public void taskIsDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
   }
 
   @Test
   public void multipleTasksAreDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Buy groceries");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
     assertThat(pageSource()).contains("Buy groceries");
   }
